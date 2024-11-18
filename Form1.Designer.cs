01012733d8ec6218de88486e15a349a43125ef64
Red Ribbon <Red Ribbon@PANIC-ROOM>
2024-11-18 09:36:13 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace SmartReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFromClipboard = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.onTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(111, 58);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(500, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(111, 112);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(500, 20);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Click += new System.EventHandler(this.txtResult_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(111, 14);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "tubereader.me";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(111, 152);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(75, 23);
+            this.btnReplace.TabIndex = 3;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(617, 56);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFromClipboard
+            // 
+            this.btnFromClipboard.Location = new System.Drawing.Point(12, 56);
+            this.btnFromClipboard.Name = "btnFromClipboard";
+            this.btnFromClipboard.Size = new System.Drawing.Size(93, 23);
+            this.btnFromClipboard.TabIndex = 5;
+            this.btnFromClipboard.Text = "from clipboard";
+            this.btnFromClipboard.UseVisualStyleBackColor = true;
+            this.btnFromClipboard.Click += new System.EventHandler(this.btnFromClipboard_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Result:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(618, 114);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Remove Extra";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onTopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // onTopToolStripMenuItem
+            // 
+            this.onTopToolStripMenuItem.Checked = true;
+            this.onTopToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.onTopToolStripMenuItem.Name = "onTopToolStripMenuItem";
+            this.onTopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.onTopToolStripMenuItem.Text = "OnTop";
+            this.onTopToolStripMenuItem.Click += new System.EventHandler(this.onTopToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 191);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFromClipboard);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSource);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFromClipboard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem onTopToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace SmartReplace
+{
+    public partial class Form1 : Form
+    {
+        static public string YOUTUBE = "https://www.youtube.com/watch?v=";
+        static public string TUBEREADER = "https://tubereader.me/videos/";
+        public Form1()
+        {
+            InitializeComponent();
+            this.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSource.Clear();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    string txt1 = txtSource.Text.Trim();
+                    if (checkBox1.Checked)
+                        txt1 = txt1.Split('?')[0];
+                    txtResult.Text = txt1.Replace(TUBEREADER, YOUTUBE);
+                    txtResult_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void txtResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtResult.SelectAll();
+                if (!String.IsNullOrEmpty(txtResult.Text))
+                {
+                    Clipboard.SetText(txtResult.Text.Trim());
+                    PopupNotifier popup = new PopupNotifier();
+                    popup.TitleText = "Copy to clipboard";
+                    popup.ContentText = "Copied to clipboard successful!";
+                    popup.BodyColor = Color.LightGreen;
+                    popup.AnimationDuration = 500;
+                    popup.Popup(); // show
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void btnFromClipboard_Click(object sender, EventArgs e)
+
+        {
+            try
+            {
+                txtSource.Text = Clipboard.GetText();
+                PopupNotifier popup = new PopupNotifier();
+                popup.TitleText = "Copy from clipboard";
+                popup.ContentText = "Pasted from clipboard successful!";
+                popup.BodyColor = Color.LightYellow;
+                popup.AnimationDuration = 500;
+                popup.Popup(); // show
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void onTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !onTopToolStripMenuItem.Checked;
+            onTopToolStripMenuItem.Checked = !onTopToolStripMenuItem.Checked;
+        }
+    }
+}
